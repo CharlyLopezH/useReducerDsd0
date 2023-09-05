d9f9f2111a4aa8b7dca84abf220321f2342c0403
Carlos Ignacio <carlosignaciolopez@gmail.com>
2023-09-05 14:03:58 -0600

    0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { StrictMode, useState } from 'react'
 import './App.css'
 import { Counter } from './components/Counter'
 import TodoList from './components/TodoList'
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,28 @@
-export const TodoList=()=>{
+import { useReducer, useState } from "react";
 
-    const onHandleClick=({target})=>{
-        console.log(target.value);
-    }
+const reducer=(state, action)=>{    
+    console.log(action.type);
+    switch(action.type) {
+        case types.delete: 
+            return state.filter(todo=>todo.id !=action.payload);           
+        case types.add:            
+            return [...state,action.payload]
+        case types.update:{ 
+            const todoEdit = action.payload;
+            return state.map(todo=>todo.id===todoEdit.id ? todoEdit:todo)
+        }            
+    default: state;
+}
+}
+
+//Types disponibles para los dispatch
+const types={
+    add: 'ADD',
+    update: 'UPDATE',
+    delete:'DELETE'
+}
+
+export const TodoList=()=>{
 
     const initialTodos = [
         {id:1, title:'Todo1'},
@@ -10,23 +30,57 @@ export const TodoList=()=>{
         {id:3, title:'Todo3'},
         {id:4, title:'Todo4'}
     ];
+
+    const [todos,dispatch]=useReducer(reducer,initialTodos);
+
+    const [text,setText]=useState('');
+
+    const onHandleSubmit =(e)=> {     
+        e.preventDefault();                
+        const newTodo = {id: Date.now(), title: text}        
+        dispatch({
+            type:types.add, 
+            payload: newTodo
+        });
+    }
+
     return(
         <>
         <div> 
             <h2> Todo List</h2>
             
             <ul>
-            {initialTodos.map(e=>
-                <li key = {e.id}> {`${e.id}.- ${e.title}`} 
+            {todos.map(todo=>
+                <li key = {todo.id}>
+                 {todo.title}
                 <button
-                value={e.id}
-                onClick={(e)=>onHandleClick(e)}
-                >Delete ({e.id}) </button>  
+                value={todo.id}
+                onClick={()=>dispatch(
+                                    {type:types.delete,
+                                     payload: todo.id})}
+                >Borrar </button>  
+                
+                <button onClick={()=>dispatch({
+                        type:types.update,
+                        payload: {...todo,title:text}
+                        })}>
+                    Update 
+               </button>  
+
                 </li>                
                 )}
             </ul>
+            <hr/>
         </div>
+        <form onSubmit={onHandleSubmit}>
+        <input 
+              placeholder="Todo"
+              value={text}
+              onChange={e=>setText(e.target.value)}
+            />
+        </form>
             </>
+            
     )
 }
 export default TodoList;
